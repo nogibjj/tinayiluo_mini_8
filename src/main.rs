d793f95156195ad3716246df4c9ea62dc47177c3
GitHub Action <action@github.com>
2023-10-21 20:51:12 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use std::env;
+use std::fs::OpenOptions; // Added this import
+use std::io::Write;
+use std::time::Instant;
 use tina_yi_sqlite::{extract, query, transform_load};
+
+const LOG_FILE: &str = "rust_query_log.md"; // Define the LOG_FILE constant
+
+fn log_query(query: &str, times: u128, mem_used: u64) -> std::io::Result<()> {
+    let mut file = OpenOptions::new().append(true).open(LOG_FILE)?;
+
+    writeln!(file, "```sql\n{}\n```\n", query)?;
+    writeln!(
+        file,
+        "the query took {} microseconds and used {} kB\n",
+        times, mem_used
+    )?;
+
+    Ok(())
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,6 +27,11 @@ fn main() {
     }
 
     let action = &args[1];
+
+    let start_time = Instant::now(); // Initialize start_time
+                                     // Assuming you have the sys_info crate for mem_info.
+    let mem_info_before = sys_info::mem_info().unwrap(); // Initialize mem_info_before
+
     match action.as_str() {
         "extract" => {
             extract(
@@ -27,6 +50,16 @@ fn main() {
                     eprintln!("Error: {:?}", err);
                 } else {
                     println!("Query executed successfully!");
+                    let end_time = Instant::now();
+                    let elapsed_time = end_time.duration_since(start_time);
+                    let mem_info_after = sys_info::mem_info().unwrap();
+                    let mem_used = mem_info_after.total - mem_info_before.total;
+
+                    match log_query(q, elapsed_time.as_micros(), mem_used) {
+                        // Removed borrowing
+                        Ok(_) => {}
+                        Err(e) => println!("Error: {:?}", e),
+                    }
                 }
             } else {
                 println!("Usage: {} query [SQL query]", args[0]);
